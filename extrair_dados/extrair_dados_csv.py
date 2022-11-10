2c59cdc34a8524dc66c0cd4bd211ee6786217f04
Italo <contatoitallo@hotmail.com>
2022-11-10 13:56:20 -0300

    chore: :sparkles: Cria database e as collections

--- extrair_dados/extrair_dados_csv.py
+++ extrair_dados/extrair_dados_csv.py
@@ -2,3 +2,8 @@ from pymongo import MongoClient
 
 client = MongoClient("mongodb://localhost:27017")
 print(client.list_database_names())
+
+db = client.Teste_Python
+collection_empresas = db.Empresas
+collection_socios = db.Socios
+collection_estabelecimentos = db.Estabelecimentos
