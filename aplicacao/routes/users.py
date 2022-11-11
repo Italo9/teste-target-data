ede2b987be3750d80e44dfc6f97ce127bb7f2532
Italo <contatoitallo@hotmail.com>
2022-11-11 10:18:34 -0300

    chore: :sparkles: Implementa rota de login, verificando se está logado, pega os dados dos inputs, lança mensagem de erro se estiver faltando a senha e o nome de login. Verifica se existe o nome no mongoDB, se sim verifica se a senha está correta e encaminha o usuário para pagina principal, se não,  informa uma mensagem e encaminha pra pagina de login novamente. Além disso, se todos os campos estiverem em branco, e clicar em Criar Conta, é encaminhado para a pagina de criar uma conta.

--- aplicacao/routes/users.py
+++ aplicacao/routes/users.py
@@ -1,10 +1,14 @@
 from flask import (
     Blueprint,
     redirect,
-    url_for,
     render_template,
     session,
+    url_for,
+    request,
+    flash,
 )
+from settings import db
+from werkzeug.security import check_password_hash
 
 user = Blueprint("user", __name__)
 
@@ -20,3 +24,46 @@ def main():
         return render_template("views_users/main.html")
     else:
         return redirect(url_for("user.index"))
+
+
+@user.route("/login", methods=["GET", "POST"])
+def login():
+    if "username" in session:
+        return redirect(url_for("user.main"))
+    elif request.method == "POST":
+        username = request.form.get("user")
+        password = request.form.get("password")
+        if (username != "") and (password == ""):
+            flash("Por favor, preencha todos os campos")
+            return render_template(
+                "views_users/login.html", type_alert="danger"
+            )
+
+        elif (username == "") and (password != ""):
+            flash("Por favor, preencha todos os campos")
+            return render_template(
+                "views_users/login.html", type_alert="danger"
+            )
+
+        elif (username != "") and (password != ""):
+            user_found = db.users.find_one({"name": username})
+            if user_found:
+                user_val = user_found["name"]
+                passwordcheck = user_found["password"]
+                if check_password_hash(passwordcheck, password):
+                    session["username"] = user_val
+                    return redirect(url_for("user.main"))
+                else:
+                    flash("Senha Incorreta", "error")
+                    return render_template(
+                        "views_users/login.html", type_alert="danger"
+                    )
+            else:
+                flash("Usuário não encontrado")
+                return render_template(
+                    "views_users/login.html", type_alert="danger"
+                )
+
+        return redirect(url_for("user.pageCreate"))
+
+    return render_template("views_users/login.html")
