edbaaf59a2b268f4b4ddb222079e3691b7aea67b
Italo <contatoitallo@hotmail.com>
2022-11-11 07:39:13 -0300

    chore: :sparkles: Implementa variável de conexão, (uma vez que foi escolhido usar Cloud tendo em vista a gratuidade de 14 dias do serviço)  e verifica a conexão com o elasticSearch

--- elastic_search/conexao_elastic_search.py
+++ elastic_search/conexao_elastic_search.py
@@ -1,3 +1,16 @@
+from elasticsearch import Elasticsearch
+from pymongo import MongoClient
 import os
 
 MONGO_URL = os.environ.get("mongodb://localhost:27017")
+
+client = MongoClient(MONGO_URL)
+db = client.Teste_Python
+
+es = Elasticsearch(
+    cloud_id="""My_deployment:dXMtY2VudHJhbDEuZ2NwLmNsb3VkLmVzLmlvJGJiY2E4ODdhNmU4MjQzY2E4ZTIz
+    ZTNmYzUwMjUzMzQ0JDllYzM2MzEyMmZkNTQ4NTA5M2E4NTY3NDk0ZDkxNGNm""",
+    http_auth=("elastic", "wxdXoUF9SA8ns30bW9pxrBI8"),
+)
+
+print(es.info())
