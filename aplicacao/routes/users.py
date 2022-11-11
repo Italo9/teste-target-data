70bbf969f3a9e29959833a0d76413e07d5f7ab2b
Italo <contatoitallo@hotmail.com>
2022-11-11 09:35:55 -0300

    chore: :sparkles: Implementa rota para acessar pagina inicial verificando se o usuário está logado

--- aplicacao/routes/users.py
+++ aplicacao/routes/users.py
@@ -2,6 +2,8 @@ from flask import (
     Blueprint,
     redirect,
     url_for,
+    render_template,
+    session,
 )
 
 user = Blueprint("user", __name__)
@@ -10,3 +12,11 @@ user = Blueprint("user", __name__)
 @user.route("/")
 def index():
     return redirect(url_for("user.login"))
+
+
+@user.route("/main")
+def main():
+    if "username" in session:
+        return render_template("views_users/main.html")
+    else:
+        return redirect(url_for("user.index"))
