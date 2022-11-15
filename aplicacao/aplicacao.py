c7265b980f43083a62c1ee683a3d447114ce5eaf
Italo <contatoitallo@hotmail.com>
2022-11-15 07:50:31 -0300

    chore: :sparkles: Implementa a função list_empresas() para verificar se está logado, caso positivo mostra a lista com os dados. No processo pega o parâmetro page e per_page da página, cria lista de cabeçalho da tabela com totalidade de 5 itens por pagina. Renderiza a pagina com todos os dados utilizando paginação e bootstrap. Se negativo, encaminha para a página de login.

--- aplicacao/aplicacao.py
+++ aplicacao/aplicacao.py
@@ -1,12 +1,12 @@
 from flask import Flask
 from routes.users import user
 
-# from routes.views import views
+from routes.views import views
 
 app = Flask(__name__)
 config_object = "settings"
 app.config.from_object(config_object)
 
 app.register_blueprint(user)
-# app.register_blueprint(views)
+app.register_blueprint(views)
 app.run()
--- aplicacao/routes/views.py
+++ aplicacao/routes/views.py
@@ -1,7 +1,40 @@
-from flask import Blueprint
+from flask import Blueprint, session, render_template
+from flask_paginate import Pagination, get_page_args
+from werkzeug.utils import redirect
+from flask.helpers import url_for
+from settings import (
+    list_keys_empresas,
+    list_all_dados_empresas,
+)
 
 views = Blueprint("views", __name__)
 
 
 def get_data(list_data, offset, per_page):
     return list_data[offset: offset + per_page]
+
+
+@views.route("/list-empresas")
+def list_empresas():
+    if "username" in session:
+        page, per_page, offset = get_page_args(
+            page_parameter="page", per_page_parameter="per_page"
+        )
+        keys_empresas = list_keys_empresas
+        total = int(len(list_all_dados_empresas))
+        pagination_empresas = get_data(
+            list_all_dados_empresas, offset, per_page
+        )
+        pagination = Pagination(
+            page=page, per_page=per_page, total=total, record_name="empresas"
+        )
+        return render_template(
+            "views/list_empresas.html",
+            head=keys_empresas,
+            title="Dados Empresas",
+            dados=pagination_empresas,
+            pagination=pagination,
+        )
+    else:
+
+        return redirect(url_for("user.login"))
